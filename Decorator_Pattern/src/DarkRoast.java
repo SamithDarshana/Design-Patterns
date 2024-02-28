0317fdcb85d6fae1b1d73109a3c65cb0ea09be2b
Samith Darshana <samithdarshana49@gmail.com>
2024-02-28 11:03:30 +0530

    Decorator Pattern

---
+++ Decorator_Pattern/src/DarkRoast.java
@@ -0,0 +1,9 @@
+public class DarkRoast extends Beverage{
+    public DarkRoast(){
+        description = "DarkRoast";
+    }
+    @Override
+    public double cost() {
+        return 0.99;
+    }
+}
